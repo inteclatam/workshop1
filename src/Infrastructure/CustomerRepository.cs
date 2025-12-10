cd49bb8a2bac38c9b8f17d7c6dfabf1acd331f7b
Edward Ceballos <edward.ceballos@gmail.com>
2025-12-10 16:19:42 -0500

    Adds customer repository implementation
    Adds a concrete implementation for customer data persistence.
    
    This implementation currently uses in-memory storage and will be
    updated to use a database in a future commit.

--- src/Infrastructure/CustomerRepository.cs
+++ src/Infrastructure/CustomerRepository.cs
@@ -1,6 +1,32 @@
-namespace DefaultNamespace;
+using Intec.Workshop1.Customers.Domain;
+using Intec.Workshop1.Customers.Domain.ValueObjects;
 
-public class CustomerRepository
+namespace Intec.Workshop1.Customers.Infrastructure;
+
+public class CustomerRepository:ICustomerRepository
 {
-    
+    public Task<Customer?> GetByIdAsync(CustomerId id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Customer?> GetByEmailAsync(EMailAddress email, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Customer customer, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Customer customer, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Customer customer, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
 }
