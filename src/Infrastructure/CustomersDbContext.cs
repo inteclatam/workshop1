ea7feeada9d4f8201c4e0c084bba5c2a56127a68
Edward Ceballos <edward.ceballos@gmail.com>
2025-12-11 10:30:02 -0500

    Added DbContext and Mappings

--- src/Infrastructure/CustomersDbContext.cs
+++ src/Infrastructure/CustomersDbContext.cs
@@ -1,8 +1,22 @@
+using Intec.Workshop1.Customers.Domain;
 using Microsoft.EntityFrameworkCore;
+using ContactInformation = Intec.Workshop1.Customers.Infrastructure.Configuration.ContactInformation;
 
 namespace Intec.Workshop1.Customers.Infrastructure;
 
 public class CustomersDbContext:DbContext
 {
-    
+    public CustomersDbContext(DbContextOptions<CustomersDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<ContactInformation> ContactInformations => Set<ContactInformation>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CustomersDbContext).Assembly);
+        
+    }
 }
