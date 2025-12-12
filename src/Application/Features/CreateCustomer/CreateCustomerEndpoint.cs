9bba56a48c23dc58f8cf041eb7eb7c899bdc8bea
Edward Ceballos <edward.ceballos@gmail.com>
2025-12-12 09:35:28 -0500

    Feat:Update CreateCustomerEndpoint implementation

--- src/Application/Features/CreateCustomer/CreateCustomerEndpoint.cs
+++ src/Application/Features/CreateCustomer/CreateCustomerEndpoint.cs
@@ -5,11 +5,25 @@ namespace Intec.Workshop1.Customers.Application.Features.CreateCustomer;
 
 public static class CreateCustomerEndpoint
 {
-    private static async Task<IResult> Handle(CreateCustomerRequest request,CommandDispatcher dispatcher, CancellationToken cancellationToken)
+    public static IEndpointRouteBuilder MapCreateCustomerEndpoint(this IEndpointRouteBuilder endpoints)
     {
-        var command = new CreateCustomerCommand(request.FirstName,request.LastName,request.EMail, request.PhoneNumber);
-      var result=  await dispatcher.DispatchAsync(command, cancellationToken);
+        endpoints.MapPost("/api/customers", async (
+    CreateCustomerRequest request,
+    CommandDispatcher dispatcher,
+    CancellationToken cancellationToken) =>
+        {
+            var command = new CreateCustomerCommand(
+                request.FirstName,
+                request.LastName,
+                request.EMail,
+                request.PhoneNumber);
 
-      return Results.Created($"/customers/{result.Id}",result);
+            var result = await dispatcher.DispatchAsync(command, cancellationToken);
+
+            return Results.Created($"/customers/{result.Id}", result);
+        }
+      );
+        return endpoints;
     }
 }
+
